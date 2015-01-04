2c97d81ab2785a96ca7e22ddb0794b477a2ee8e4
Andreas Nordahl <andnorda@gmail.com>
2015-01-04 13:26:18 +0100

    repo url

--- src/main/java/codecount/domain/GitRepo.java
+++ src/main/java/codecount/domain/GitRepo.java
@@ -1,20 +1,29 @@
 package codecount.domain;
 
+import org.eclipse.jgit.api.Git;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 public class GitRepo {
     private Collection<GitFile> files;
+    private String url;
 
-    public GitRepo(File root) {
-        files = Arrays.asList(root.listFiles()).stream()
+    public GitRepo(File root) throws IOException {
+        files = Arrays.asList(root.listFiles(file -> !file.getName().equals(".git"))).stream()
                 .map(file -> new GitFile(file))
                 .collect(Collectors.toList());
+        url = Git.open(root).getRepository().getConfig().getString("remote", "origin", "url");
     }
 
     public Collection<GitFile> getFiles() {
         return files;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }
--- src/test/java/codecount/domain/GitRepoTest.java
+++ src/test/java/codecount/domain/GitRepoTest.java
@@ -1,5 +1,8 @@
 package codecount.domain;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -10,6 +13,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GitRepoTest {
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
+    private Git git;
+
+    @Before
+    public void setUp() throws Exception {
+        git = Git.init().setDirectory(folder.getRoot()).call();
+    }
 
     @Test
     public void returns_files() throws Exception {
@@ -21,5 +30,19 @@ public class GitRepoTest {
 
         // Then
         assertThat(gitRepo.getFiles().size(), is(1));
+    }
+
+    @Test
+    public void url_is_origin_url() throws Exception {
+        // Given
+        StoredConfig config = git.getRepository().getConfig();
+        config.setString("remote", "origin", "url", "http://github.com/github/testrepo.git");
+        config.save();
+
+        // When
+        GitRepo gitRepo = new GitRepo(folder.getRoot());
+
+        // Then
+        assertThat(gitRepo.getUrl(), is("http://github.com/github/testrepo.git"));
     }
 }
